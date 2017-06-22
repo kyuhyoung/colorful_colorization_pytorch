a43468277ff99905f7bac05308f2ce3b0eb442b4
kyuhyoung <kyuhyoung@gmail.com>
2017-06-23 05:36:13 +0900

    Add Net and reference git repository

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
+# reference : https://github.com/tdeboissiere/DeepLearningImplementations/blob/master/Colorful/src/model/models_colorful.py
+
+import torch.nn as nn
 import torchvision.transforms as transforms
 from os.path import join, exists, abspath, basename
 from torchvision.datasets.utils import check_integrity, download_url
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 64, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        t1 = self.num_flat_features(x)
+        x = x.view(-1, self.num_flat_features(x))
+        #x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
 
 def check_if_uncompression_done(dir_save):
 
